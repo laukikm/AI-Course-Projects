f85c7e73e73b411f19dd5f7925b6eca291bce015
laukikm <laukikm11@gmail.com>
2020-02-25 13:54:05 -0700

    Added ai_env

--- ai_env/bin/activate.fish
+++ ai_env/bin/activate.fish
@@ -57,7 +57,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/laukik/ai_projects/ai_env"
+set -gx VIRTUAL_ENV "/home/laukik/AI-Course-Projects/ai_env"
 
 # https://github.com/fish-shell/fish-shell/issues/436 altered PATH handling
 if test (echo $FISH_VERSION | tr "." "\n")[1] -lt 3
